1c06b342a428db555cd3eba0d69106c9268fc2e1
ghbh1 <mgbsblabla@gmail.com>
2026-07-09 18:32:15 +0300

    2nd test

--- Assets/ProjectScripts/SerializedData.cs
+++ Assets/ProjectScripts/SerializedData.cs
@@ -6,9 +6,13 @@ public class SerializedData
 {
     [SerializeField]
     private int totalCoins = 0;
+    [SerializeField]
     private int highestScore = 0;
+    [SerializeField]
     private bool controls  = true;
+    [SerializeField]
     private float SfxVolumeValue = 1;
+    [SerializeField]
     private float MusicVolumeValue = 1;
 
 }
